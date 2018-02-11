4f309a04d6f61b37c7fa3cd2dcd27e8aef6fe8fb
renichinohe <renichinohe@icloud.com>
2018-02-11 22:23:21 +0900

    ToStringのテストを追加

---
+++ src/test/groovy/ToStringTest.groovy
@@ -0,0 +1,20 @@
+import annotation.User
+import normal.NormalUser
+import spock.lang.Specification
+
+class ToStringTest extends Specification{
+
+    def test(){
+
+        setup:
+        String user = User.createUser().toString()
+        String normalUser = NormalUser.createNormalUser().toString()
+        String exp = "User(id=UserId(value=1), name=UserName(太郎), address=Address(東京))"
+
+        expect:
+        user == normalUser
+        user == exp
+        normalUser == exp
+
+    }
+}
